37dad2755a1fc0ed059f10aa24678da4949c6711
Kangeldi Eginbaev <han.eginbaev@gmail.com>
2023-05-11 17:02:44 +0600

    add .gitignore

---
+++ task-2/board.ts
@@ -0,0 +1,7 @@
+const white = '  ';
+const black = '██'
+
+for (let i = 0; i < 4; i++) {
+    console.log(black + white + black + white + black + white + black + white);
+    console.log(white + black + white + black + white + black + white + black);
+}
